f72b7b0ed757efe197a121ef922ba2a3b693799c
saraswat2385 <saraswat2385@gmail.com>
2018-07-03 10:23:24 +0530

    WIP

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
- docker build -t frontend:latest -f ./Dockerfile1 .
- docker build -t backend:latest -f ./Dockerfile .
+ docker build -t saraswat2385/frontend:latest -f ./Dockerfile1 .
+ docker build -t saraswat2385/backend:latest -f ./Dockerfile .
